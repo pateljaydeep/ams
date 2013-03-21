845727dfba47aa4ffa135979cb35f90b08b1fe2b
subramantr <Subramania.Rao@deem.com>
2013-03-21 04:05:22 -0700

    Add roles to access this application

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     userLoginId = params[:name]
     pwd = params[:password]
+    authIssue = nil;
     begin
       response = RestClient.get("https://atlas:8443/atlas/rest/auth?user=#{userLoginId}&pwd=#{pwd}")
       
@@ -14,15 +15,21 @@ class SessionsController < ApplicationController
       
       empId = jsonResp["id"]
     rescue => e
-      puts('Exception during REST call for authentication ')
-      e.response
+      puts('Exception during authentication ')
+      authIssue = true
     end
 
     if userLoginId[0] != nil and empId != nil
-      session[:user_id] = empId
-      session[:user_name] = jsonResp["name"]
-      
-      redirect_to assets_url, notice: "You are logged in as #{userLoginId} "
+      authorizedRoles = ['admin', 'user-30', 'user-20', 'user-10']
+      if authorizedRoles.include? jsonResp["role"]
+        session[:user_id] = empId
+        session[:user_name] = jsonResp["name"]
+        redirect_to assets_url, notice: "You are logged in as #{userLoginId} "
+      else
+        redirect_to login_url, alert: "Unauthorized Login"
+      end
+    elsif authIssue
+      redirect_to login_url, alert: "Unable to authenticate"
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
