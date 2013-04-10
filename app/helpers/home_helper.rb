59a4c8d45de6b7d774adeb63577d1786a8ab5969
Jaydeepkumar R Patel <jaydeepkumar.patel@deem.com>
2013-04-10 09:58:08 -0700

    solved issue in helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,6 @@
 module HomeHelper
   def find_asset_type(name, asset_types)
-    index = asset_types.find_index { |asset_type| asset_type.name = name}
+    index = asset_types.find_index { |asset_type| asset_type.name == name}
     asset_types[index]
   end
 end
