b047aa5b0215108a6366667b4fc53657760434c3
Jaydeepkumar R Patel <jaydeepkumar.patel@deem.com>
2013-04-19 02:34:54 -0700

    detail page changes

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,10 +2,16 @@ class Asset < ActiveRecord::Base
   attr_accessible :asset_documents, :asset_id, :asset_type_id, :asset_image, :description, :display_size, :make_year, :manufacturer, :model, :operating_system, :purchase_date, :release_date, :serial_number
   self.per_page = 10
   
-  validates :asset_id, :presence => true;
-  validates :purchase_date, :presence => true;
-  validates :serial_number, :presence => true;
-  validates :make_year, :presence => true;
+  validates :asset_id, :presence => true, length: { maximum: 20}
+  validates :purchase_date, :presence => true
+  validates :serial_number, :presence => true, length: { maximum: 25}
+  validates :make_year, :presence => true, length: { maximum: 4}, numericality: true
+  validates :description, length: {maximum: 200}
+  validates :display_size, length: {maximum: 10}
+  validates :manufacturer, length: {maximum: 20}
+  validates :model , length: {maximum: 15}
+  validates :operating_system, length: {maximum: 15}
+  
   belongs_to :asset_type
   has_one :asset_assignment
   has_many :asset_allocation_histories
