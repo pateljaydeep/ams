52e316d5d7db310f4f9a3188211b2c68524c3d2e
Jaydeepkumar R Patel <jaydeepkumar.patel@deem.com>
2013-06-08 23:35:36 -0700

    Asset pipeline

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_via, :remote_cache
 set :branch, 'master'
 set :scm_verbose, true
 set :use_sudo, false
-set :rails_env, :development
+set :rails_env, :production
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
